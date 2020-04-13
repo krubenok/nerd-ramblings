e1cd4fa3949ae07d29e3cf5621c142121fa340bf
Kyle Rubenok <kyle@rubenok.ca>
2020-04-13 00:11:08 -0400

    Update favicons

--- gatsby-config.js
+++ gatsby-config.js
@@ -87,52 +87,38 @@ module.exports = {
     },
     `gatsby-plugin-offline`,
     `gatsby-plugin-netlify`,
-    // {
-    //   resolve: `gatsby-plugin-feed`,
-    //   options: {
-    //     query: `
-    //       {
-    //         site {
-    //           siteMetadata {
-    //             title: siteTitle
-    //             description: siteDescription
-    //             siteUrl
-    //           }
-    //         }
-    //       }
-    //     `,
-    //     feeds: [
-    //       {
-    //         serialize: ({ query: { site, allPost } }) => {
-    //           return allPost.nodes.map((post) => {
-    //             return {
-    //               title: post.title,
-    //               date: post.date,
-    //               excerpt: post.excerpt,
-    //               url: site.siteMetadata.siteUrl + post.slug,
-    //               guid: site.siteMetadata.siteUrl + post.slug,
-    //               custom_elements: [{ "content:encoded": post.html }],
-    //             }
-    //           })
-    //         },
-    //         query: `
-    //           {
-    //             allPost(sort: { fields: date, order: DESC }) {
-    //               nodes {
-    //                 title
-    //                 date(formatString: "MMMM D, YYYY")
-    //                 excerpt
-    //                 slug
-    //                 html
-    //               }
-    //             }
-    //           }
-    //         `,
-    //         output: `rss.xml`,
-    //         title: `Nerd Ramblings by Kyle Rubenok`,
-    //       },
-    //     ],
-    //   },
-    // },
+    `gatsby-plugin-sharp`,
+    `gatsby-remark-images`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/pages`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/posts`,
+      },
+    },
   ],
 }
