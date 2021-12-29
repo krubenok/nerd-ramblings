fda7cc38ccae8b27c7787dc499a724da3314243a
Kyle Rubenok <kyle@rubenok.ca>
2021-12-28 17:08:17 -0800

    add offline and image optimization support

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,7 @@ module.exports = {
       resolve: "@lekoarts/gatsby-theme-minimal-blog",
       options: {
         showLineNumbers: true,
+        mdx: false,
         feed: true,
         feedTitle: "Nerd Ramblings by Kyle Rubenok",
         navigation: [
@@ -70,7 +71,40 @@ module.exports = {
     },
     `gatsby-plugin-sitemap`,
     `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-transformer-remark`,
     `gatsby-plugin-mdx-embed`,
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "Nerd Ramblings by Kyle Rubenok",
+        short_name: "Nerd Ramblings",
+        description:
+          "Written by Kyle Rubenok. I'm a PM at Microsoft. Previously McGill CS, President at McGill CSUS, Co-Director at HackMcGill and Founding Partner at Penguinleaf.",
+        start_url: "/",
+        background_color: "#fafafa",
+        theme_color: "#a625a4",
+        display: "standalone",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        precachePages: [`/about/`, `/blog/*`, `/gear/`],
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -141,6 +175,19 @@ module.exports = {
             `,
             output: `rss.xml`,
             title: `Nerd Ramblings - Kyle Rubenok`,
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+            },
           },
         ],
       },
